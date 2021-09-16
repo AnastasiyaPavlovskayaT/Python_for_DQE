123f8c627e9d141847a71617d6f8586379853366
AnastasiyaPavlovskayaT <at.pavlovskaya@gmail.com>
2021-09-16 22:09:52 +0300

    update script using pep 8

--- module-2-python-basics/hometask_module_2.py
+++ module-2-python-basics/hometask_module_2.py
@@ -10,22 +10,27 @@ import string
 # the collections module is imported
 import collections
 
-# creation of a list of random number of dicts (from 2 to 10); dict's random numbers of keys is letter; dict's values should be a number (0-100),
-random_list_of_dicst = [{random.choice(string.ascii_letters):random.randint(0,100) for i in range(random.randint(0,10))} for i in range(2,random.randint(2,10))]
+# creation of a list of random number of dicts (from 2 to 10)
+# dictionaries random numbers of keys is letter; dictionaries values should be a number (0-100),
+random_list_of_dict = [{random.choice(string.ascii_letters): random.randint(0, 100)
+                        for index_couple in range(random.randint(0, 10))}
+                       for index_dict in range(2, random.randint(2, 10))]
 
-# the test data to check the algorithm: random_list_of_dicst =  [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
+# the test data to check the algorithm: random_list_of_dict =  [{'a': 5, 'b': 7, 'g': 11}, {'a': 3, 'c': 35, 'g': 42}]
 
-# generted random_list_of_dicst list output
-print('Сreated list of random number of dicts:\n ',random_list_of_dicst)
+# generated random_list_of_dict list output
+print('Created list of random number of dicts:\n ', random_list_of_dict)
 
-# announce auxiliary dict; defaultdict is an inherited class
-#default items are created with list (), which returns a new empty list object
+# announce auxiliary dict using the default dict is an inherited class
+# default items are created with list (), which returns a new empty list object
 auxiliary_dict = collections.defaultdict(list)
 
-# merging all the generated dictionaries of the list into one dict by keys - values ​​are the lists of values ​​of merged dicts
-# defaultdict auxiliary_dict is using in the algorithm because we need to get an element with a key that is not currently in the dictionary
-for dict_element in random_list_of_dicst:
-    for key,value in dict_element.items():
+# merging all the generated dictionaries of the list into one dict by keys
+# values are the lists of values of merged dicts
+# default dict auxiliary_dict is using in the algorithm because
+# we need to get an element with a key that is not currently in the dictionary
+for dict_element in random_list_of_dict:
+    for key, value in dict_element.items():
         auxiliary_dict[key].append(dict_element[key])
 
 # print(auxiliary_dict)
@@ -35,13 +40,12 @@ common_dict = {}
 
 # find the maximum value in the list for each key and its index
 # add the updated key value and maximum value to the common dict
-for key_letter,list_value in auxiliary_dict.items():
+for key_letter, list_value in auxiliary_dict.items():
     if len(list_value) > 1:
-        common_dict.update({key_letter+str('_')+str(list_value.index(max(list_value))+1):max(list_value)})
+        common_dict.update({key_letter + str('_') + str(list_value.index(max(list_value)) + 1): max(list_value)})
     else:
         common_dict.update({key_letter: max(list_value)})
 
 # common dict output
-print('Resulted common dict:\n', common_dict)
-
 # for test data common dict should be {'a_1': 5, 'b': 7, 'c': 35, 'g_2': 42}
+print('Resulted common dict:\n', common_dict)
